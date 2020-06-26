57128c48aa00ae399244f596d4de248f18ba5a2a
NeguliSimanis <simanism@inbox.lv>
2020-06-26 20:39:20 +0300

    added player and ground objects

--- Zapte/Assets/Scripts/UserInterfaceManager.cs
+++ Zapte/Assets/Scripts/UserInterfaceManager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserInterfaceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    Button startGame;
+    [SerializeField]
+    Button quitGame;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
