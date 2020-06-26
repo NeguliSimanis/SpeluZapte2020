96eeb0f3a95e0b96172a4767320e00995de2a279
NeguliSimanis <simanism@inbox.lv>
2020-06-26 21:03:58 +0300

    player can jump now

---
+++ Zapte/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Checks if player is located on ground or flying/jumping
+/// </summary>
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField]
+    PlayerController playerController;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            playerController.isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            playerController.isGrounded = false;
+        }
+    }
+}
--- Zapte/Assets/Scripts/Player/PlayerController.cs
+++ Zapte/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D playerRigidbody2D;
+
+    #region jumping data
+    [HideInInspector]
+    public bool isGrounded;
+    float jumpForce = 200f;
+    #endregion
+
+    #region animation data
+    [SerializeField]
+    Animator playerAnimator;
+    #endregion
+
     void Start()
     {
-        
+        isGrounded = false;
+        playerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckPlayerInput();
+    }
+
+    private void LateUpdate()
+    {
+        // update animations
+        playerAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void CheckPlayerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("JUMP PRESSED");
+            playerRigidbody2D.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+        }
     }
 }
