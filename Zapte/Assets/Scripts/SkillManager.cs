c7224cb0ad6ef24b91271e955ae5cc94ad1b2662
WabbajackLV <bleachlv@inbox.lv>
2020-06-27 17:18:26 +0300

    Made skill tree visualization
    Now we have a skill tree in main menu, where we can click on any skill in the tree and gain info about the skill

---
+++ Zapte/Assets/Scripts/SkillManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+	public static SkillManager instance; //MARKER singleton Pattern
+	
+	public Skill[] skills;
+	public SkillButton[] skillButtons;
+  
+	public Skill activateSkill;
+  
+	private void Awake(){
+		if(instance == null){
+			instance = this;
+		}else{
+			if(instance != this){
+				Destroy(gameObject);
+			}
+		}
+		DontDestroyOnLoad(gameObject);
+		
+	}
+  
+}
